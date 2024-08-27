0db102f2d20eeb3cf1d71928d65e14b40bb1e503
jagadeesh p <jagadeeshpc5@gmail.com>
2024-08-27 18:36:50 +0530

    adding colors to redis

--- redis.sh
+++ redis.sh
@@ -1,8 +1,13 @@
+echo -e "\e[36m>>>>>>>>install redis repos <<<<<<<<<<<<\e[0m"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>>>>install redis <<<<<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
 
+echo -e "\e[36m>>>>>>>>update redis listen address <<<<<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
+echo -e "\e[36m>>>>>>>>start redis service <<<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl start redis
